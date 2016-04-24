8fec499bddc9ec5837f993718794ef2ab9a68a9f
LiuYi <yl3068@nyu.edu>
2016-04-23 22:22:10 -0400

    finally it works XD

--- query_sentences.lua
+++ query_sentences.lua
@@ -4,6 +4,15 @@ require('base')
 require('io')
 require('torch')
 
+--reset start state to zeros
+function reset_state()
+    if model ~= nil and model.start_s ~= nil then
+        for d = 1, 2 * num_layers do
+            model.start_s[d]:zero()
+        end
+    end
+end
+
 --readline from user
 function readline()
 	local line = io.read("*line")
@@ -21,20 +30,24 @@ function getIndex(line)
 			ind_seq[i] = word2ind["<unk>"]
 		else
 			ind_seq[i] = word2ind[line[i]]
+		end
+	end
 	return ind_seq
 end
 
 function makePrediction(ind_seq)
 	local known_len = #ind_seq-1
-	local pred_len = ind_seq[1]
-	--TO DO!
-	reset_state()
+	local pred_len = tonumber(ind_seq[1])
+
+	--initialize start state
+	reset_state(num_layers)
 	g_disable_dropout(model.rnns)
+	g_replace_table(model.s[0], model.start_s)
 
 	--loop the known part
 	for i = 2, known_len do
-		local x = ind_seq[i]
-		local y = ind_seq[i+1]
+		local x = torch.DoubleTensor(batch_size):fill(ind_seq[i])
+		local y = torch.DoubleTensor(batch_size):fill(ind_seq[i+1])
 		_, model.s[1], _ = unpack(model.rnns[1]:forward({x,y,model.s[0]}))
 		g_replace_table(model.s[0], model.s[1])
 	end
@@ -42,14 +55,13 @@ function makePrediction(ind_seq)
 	--setup a tensor to store prediction results
 	local pred_seq = torch.Tensor(pred_len)
 	--make prediction
-	local x = ind_seq[known_len+1] --initialize x as the last word of input.
-	local y = ind_seq[known_len+1] --since we won't use y further, give any number would be fine.
+	local x = torch.DoubleTensor(batch_size):fill(ind_seq[known_len+1])
+	local y = torch.DoubleTensor(batch_size):fill(ind_seq[known_len+1])
 	for i = 1, pred_len do
-		local x = ind_seq[i]
 		_, model.s[1], pred = unpack(model.rnns[1]:forward({x,y,model.s[0]}))
 		_, pred_tensor = torch.max(pred, 2)
-		x = pred_tensor[1][1]
-		pred_seq[i] = x
+		x = torch.DoubleTensor(batch_size):fill(pred_tensor[1][1])
+		pred_seq[i] = pred_tensor[1][1]
 		g_replace_table(model.s[0], model.s[1])
 	end
 	return pred_seq
@@ -65,16 +77,22 @@ function outputPrediction(input_seq, pred_seq)
 		local word = ind2word[ind]
 		output = output .. " " .. word
 	end
+	print("output...")
+	print(output)
 end
 
+--load model and lexicon
 print("==> Loading model ...")
 local model_file = "./model/model.net"
-local model = torch.load(model_file)
+model = torch.load(model_file)
 print("==> Loading lexicon ...")
-local word2ind_file = "./map/"
-local ind2word_file = "./map/"
-local word2ind = torch.load(word2ind_file)
-local ind2word = torch.load(ind2word_file)
+local word2ind_file = "./map/word2ind.t7"
+local ind2word_file = "./map/ind2word.t7"
+word2ind = torch.load(word2ind_file)
+ind2word = torch.load(ind2word_file)
+
+num_layers=2 --the same with settings in main.lua
+batch_size=20 --the same with settings in main.lua
 
 while true do
 	print("Query: len word1 word2 etc")
@@ -91,6 +109,6 @@ while true do
 	else
 		local ind_seq = getIndex(line)
 		local pred_seq = makePrediction(ind_seq)
-		outputPrediction()
+		outputPrediction(line, pred_seq)
 	end
 end
